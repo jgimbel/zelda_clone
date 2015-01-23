9b9424e2b1f943b309619a76957fbcfae1452725
Nathan Bland <nathan.bland@gmail.com>
2015-01-22 22:48:27 -0800

    moved around sprites to be in correct location

--- map.py
+++ map.py
@@ -7,10 +7,10 @@ TILESIZE = 32
 MAPWIDTH = 10
 MAPHEIGHT = 10
 textures = {
-    DIRT: pygame.image.load('4_earthTiles/38earth8.bmp'),
-    GRASS: pygame.image.load('1_grassTiles/1grass1.bmp'),
-    WATER: pygame.image.load('2_waterTiles/13water3.bmp'),
-    COAL: pygame.image.load('4_earthTiles/32earth2.bmp')
+    DIRT: pygame.image.load('sprites/earthTiles/38earth8.bmp'),
+    GRASS: pygame.image.load('sprites/grassTiles/1grass1.bmp'),
+    WATER: pygame.image.load('sprites/waterTiles/13water3.bmp'),
+    COAL: pygame.image.load('sprites/earthTiles/32earth2.bmp')
 }
 class Map:
     resources = [DIRT, GRASS, WATER, COAL]
