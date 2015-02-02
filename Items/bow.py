1c3cc62407ade1e7b921b791931efa673cc60c0b
Joel Gimbel <jgimbel@live.com>
2015-02-01 20:37:16 -0600

    freaking nathan...

--- Items/bow.py
+++ Items/bow.py
@@ -1,9 +1,8 @@
 __author__ = 'joel'
-import pygame
 
 from item import Item
 
 
 class bow(Item):
     def __init__(self):
-        super(pygame.sprite.Sprite,self).__init__("Sprites/items/bow.png")
+        super(bow, self).__init__("Sprites/items/bow.png")
